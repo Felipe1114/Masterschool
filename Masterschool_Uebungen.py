0183fe30f95793ace9165b9a4ed6f45489da02e5
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2024-09-23 12:18:59 +0200

    Ich habe einen Input hinzugefügt.

--- Masterschool_Uebungen.py
+++ Masterschool_Uebungen.py
@@ -6,3 +6,9 @@ liste1.append(43)
 liste1.insert(1, 33)
 liste1.insert(3, "test")
 print(liste1)
+
+#ich kann elemente aus einer lsite herausholen, in dem ich diesen trick anwende
+print(liste1[:1]+liste1[2:3]+liste1[5:6])
+#hier durch werden nur bestimmte objekte aus der lsite herausgeholt,
+#ohne dass die lsite zu einem String wird.
+
