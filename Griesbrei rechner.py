273a9e9668ec66ae1f6a6d8ba157c91efd5156b8
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2024-09-23 19:16:05 +0200

    funktion für Personenanzahl erstellt. unterstes Print statement entfert

--- Griesbrei rechner.py
+++ Griesbrei rechner.py
@@ -1,25 +1,19 @@
-def personen_anzahl_oder_milch_zu_grieß(personen_oder_umrechnen):
-    if personen_oder_umrechnen == "personen":
-        anzahl_personen = ("Für wie viele Personen kochst du? ")
-        milch_menge = anzahl_personen * 250
-        menge_grieß = anzahl_personen * 38
-
-
-    elif personen_oder_umrechnen == "umrechnen":#
-        pass
-    else:
-        print("falscher Input")
+def wie_viel_für_personen():
+    anzahl_personen = input("Für wie viele Personen kochst du? ")
+    milch_menge = int(anzahl_personen) * 250
+    menge_grieß = int(anzahl_personen) * 37.5
+    print(f"{milch_menge} ml Milch und {menge_grieß} g Grieß, ergeben {anzahl_personen} Portionen")
 
 milch_menge = 1000
-menge_grieß = 250
+menge_grieß = 150
 anzahl_personen = 4
-
 personen_oder_umrechnen = input("Personenanzahl oder umrechnen? ")
 
+if personen_oder_umrechnen == "Personen":
+    wie_viel_für_personen()
+elif personen_oder_umrechnen == "umrechnen":
+    pass
 
 
 
 
-
-
-print(f"{milch_menge} ml und {menge_grieß} g Grieß, ergeben {anzahl_personen} Portionen")
