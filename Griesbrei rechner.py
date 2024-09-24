36b94485a58d17062ead88916e5f67332e07e853
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2024-09-24 10:28:04 +0200

    Loop eingebaut und angefangen alles in eine einzige Funktion zu packen.

--- Griesbrei rechner.py
+++ Griesbrei rechner.py
@@ -1,16 +1,43 @@
+'''Feste und nicht veränderbare Variablen'''
+
 GRIESS = "Grieß"
 MILCH = "Milch"
 ZURUECK = "Zurück"
 
+'''------------------------------------------------------------------------------------------------------------------'''
+
+#Fragt ab, was man will: Menge für Personenzahl, oder Gries oder Milch Menge herausfinden'''
+
 def input_abfrage():# fragt den aller ersten input ab -> personen oder mengenrechner
     personen_oder_umrechnen = input("Personenanzahl oder umrechnen? ")
-    return personen_oder_umrechnen
+    if personen_oder_umrechnen == "Personen":
+        wie_viel_fuer_personen()
+    elif personen_oder_umrechnen == "umrechnen":
+        Griess_oder_Milch = input("Brauchst du die Milch- oder Griessmenge? ")
+        griess_und_milch(Griess_oder_Milch)
+    else:
+        print("falscher input")
+
+'''------------------------------------------------------------------------------------------------------------------'''
+#Abhängig der Personen wird die Menge an Milch und Grieß zurück gegeben.
+#Wie kann ich eine If funktion machen, die abhängig vom Typen funktioniert?
 
 def wie_viel_fuer_personen():# Funktion fuer Griess und Milchmenge abhaengig der Personen
     anzahl_personen = input("Fuer wie viele Personen kochst du? ")
-    milch_menge = int(anzahl_personen) * 250
-    menge_griess = int(anzahl_personen) * 37.5
-    print(f"{milch_menge} ml Milch und {menge_griess} g Griess, ergeben {anzahl_personen} Portionen")
+    is_int = 1
+    anzahl_personen_int = int(anzahl_personen)
+    if anzahl_personen_int.isdigit() == is_int.isdigit():
+        milch_menge = int(anzahl_personen) * 250
+        menge_griess = int(anzahl_personen) * 37.5
+        print(f"{milch_menge} ml Milch und {menge_griess} g Griess, ergeben {anzahl_personen} Portionen")
+    elif anzahl_personen == ZURUECK:
+        input_abfrage()
+    else:# wenn der input weder bei "if" noch bei "elif" passt, dann wird noch mal der input abgefragt
+        print("falscher input")
+        wie_viel_fuer_personen()
+
+'''------------------------------------------------------------------------------------------------------------------'''
+#Fragt ab, obich Milch habe und die Menge an Grieß dafür haben möchte, oder eben anders herum.
 
 def griess_und_milch(Griess_oder_Milch):# Funktion fuer die Umrechnng von Griess und Milch zueinander
     if Griess_oder_Milch == GRIESS:#fragt ab, ob man die Grieß menge kennt
@@ -21,8 +48,8 @@ def griess_und_milch(Griess_oder_Milch):# Funktion fuer die Umrechnng von Griess
         menge_griess = input("Wie viel Griess hast du(nur die menge)? ")
         menge_milch = griess_zu_milch(menge_griess)
         print(f"Bei {menge_griess}g Grieß braubhst du {menge_milch}ml Milch, guten Apetit.")
-    elif Griess_oder_Milch == ZURUECK:
-
+    elif Griess_oder_Milch == ZURUECK:# geht wieder zurück in die algemeine Abfrage
+        input_abfrage()
     else:# gibt den loop zurück zur abfrage nach Grieß oder Milch
         print("falscher input, bitte versuche es nochmal")
         griess_und_milch(input("Brauchst du die Milch- oder Griessmenge? Bitte nur 'Milch' oder 'Grieß' eingeben "))
@@ -37,21 +64,13 @@ def milch_zu_griess(menge_milch):# rechnet aus, wie viel g Griess ich braiche, w
 #--------------------------------------
 '''Diese Fukntion ist das hauptprogramm'''
 def grieß_koch_helfer():# die main funktion, in der alle anderen funktionen enthalten sind.
-    input_abfrage():
+    input_abfrage()
 '''Diese Funktion ist das hauptprogramm'''
 #--------------------------------------
 # Ab hier fängt der eigentlich Code an
 
-'''Dieser Abschnitt ist '''
 grieß_koch_helfer()
 
-if personen_oder_umrechnen == "Personen":
-    wie_viel_fuer_personen()
-elif personen_oder_umrechnen == "umrechnen":
-    Griess_oder_Milch = input("Brauchst du die Milch- oder Griessmenge? ")
-    griess_und_milch(Griess_oder_Milch)
-else:
-    print("falscher input")
 
 
 
