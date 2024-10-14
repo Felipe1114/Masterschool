93d53da33b8d624b0d3184809a3d138f3c240c2f
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2024-10-14 16:49:21 +0200

    viel, sehr viele files hochgeladen. ABER vor allem "romoe und julia" fertig gemacht

---
+++ algorytmus_übung.py
@@ -0,0 +1,16 @@
+# größter gemeinsamer teiler
+
+# def gcd_euclidean_algo(a, b)
+#     if b == 0:
+#         return a
+#     return gcd_euclidean_algo(b, a % b)
+#
+#
+# def test_gcd_euclidean_algo():
+#
+
+
+# übungen:
+# kleinster gemeinster teiler
+# ist ein objekt eine permuation von objekt 2 ( abc ==  cba)
+# ist eine nummer ein palindrom (3223)
--- codio_prime_minister.py
+++ codio_prime_minister.py
@@ -4,15 +4,16 @@
 # dann die zahl durch die hälfte aller vorheringen ungeraden zahlen teilen
     # warum nur die hälfte:
     # bsp: 0 - 100: 51 passt nicht mehr 2 mal in die hundert rein
+
+
+
 def make_parameters(range_list):
     prime_numbers = []
-    is_True = is_one_or_two_in_range(range_list)
-    if is_True == True:
-        return prime_numbers = [1, 2]
+    is_two_in_range(range_list, prime_numbers)
     odd_numbers = get_all_odd_numbers(range_list)
     middle_index = divide_len_odd_numbers(odd_numbers)
     list_for_dividing = split_odd_numbers(odd_numbers, middle_index)
-    iterate_thrue_odd_numbers(list_for_dividing)
+    iterate_thrue_odd_numbers(odd_numbers, list_for_dividing, prime_numbers)
 
 def get_range():
     range_start = int(input("Enter start range: "))
@@ -32,31 +33,32 @@ def get_all_odd_numbers(range_list):
         if i % 2 != 0: odd_numbers.append(i)
     return odd_numbers
 
-def iterate_thrue_odd_numbers(odd_numbers, list_for_dividing):
-    for index_odd_numbers in range(len(odd_numbers)):
-        for index_of_divider in range(len(list_for_dividing)):
-            is_odd_greater = is_odd_number_greater_divider(odd_numbers[index_odd_numbers], list_for_dividing[index_of_divider])
-            if is_odd_greater == True:
-                if odd_numbers[index_odd_numbers] % list_for_dividing[index_of_divider] == 0:
-                    pass
-                else:
-                    print()
-    # hier komme ich grade nicht weietr
-    # ich gehe hier alle zahelen druch und teile sie mit den möglichen ungeraten zahlen
-    # ich muss noch die funktion einbauchen, mit der ich richtig teile
-    # es ist wichtig, dass wirklich alle zaheln mit allen möglichen zaheln geteilt werdene
+def iterate_thrue_odd_numbers(odd_numbers, list_for_dividing, prime_numbers):
+    for index_numbers in range(len(odd_numbers)):
+        for index_divider in range(len(list_for_dividing)):
+            if odd_numbers[index_numbers] > list_for_dividing[index_divider] :
+                is_number_prime(odd_numbers, list_for_dividing, index_numbers, index_divider, prime_numbers)
+            else:
+                break
+    for number in prime_numbers:
+        print(f"the number {number} is prime")
 
+def is_number_prime(odd_numbers, list_for_dividing, index_numbers, index_divider, prime_numbers):
+    if odd_numbers[index_numbers] % list_for_dividing[index_divider] == 0:
+        return None
+    elif index_divider == len(list_for_dividing) - 1:
+        prime_numbers.append(odd_numbers[index_numbers])
+    else:
+        pass
 
-def is_one_or_two_in_range(range_list):
-    return 1 in range_list or 2 in range_list
-
-def is_divider_greater_odd_number(odd_number, divider):
-    return odd_number > divider
-
+def is_two_in_range(range_list, prime_numbers):
+    for i in range_list:
+        if i == 2:
+            prime_numbers.append(2)
 
 
 def main():
-    make_parameters(get_range())
+    make_parameters([1, 100]) # hier muss "get_range" rein. für den test ist hier was anderes
 
 if __name__ == "__main__":
     main()
