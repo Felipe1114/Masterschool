95a259e20bf0e619ef103a3423700601d17cb44a
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2024-10-31 15:05:53 +0100

    wenn mehrere filme das beste ranking haben, werden alle diese filme angezeigt

--- Codio/codio_movies.py
+++ Codio/codio_movies.py
@@ -1,28 +1,16 @@
 import random
-from re import search
 
-from macholib.mach_o import lc_str
 
-'''  Aufgaben:
-  x Menu
-  x nach auswahl wieder zurück zu menu
-  x Alle Filme auflisten
-  x einen Film hinzufügen
-  x einen film löschen
-  x einen film updaten (die wertung ändern)
-  x die statistiken anzeigen:
-  x - durchschnitt, median, bester, schlechtester
-  x einen zufälligen film printen
-  nach einem film suchen (case insensitiv)
-  x filme nach rating anzeigen
-  '''
 def search_movie(movies: type(dict)):
+    '''input: dict movies; output: none
+    searching a movie with an input
+    the search is case insensitiv, with the funktion .lower()
+    when, finished, goes back to the menu'''
     searched_movie = input("Wich movie are you searching for?: ")
     for key in movies.keys():
         if searched_movie.lower() == key.lower():
             print_single_ranked_movie(key, movies)
-    what_to_do(int(print_menu(back_to_menu())), movies)
-
+    execute_user_input(int(print_menu(back_to_menu())), movies)
 
 
 def print_single_ranked_movie(key, movies: type(dict)):
@@ -34,6 +22,7 @@ def print_ranked_movies(dic_sorted_movies):
     print("Here are al Movies, ranked from best to worst:")
     for key in dic_sorted_movies:
         print(key, dic_sorted_movies[key], sep = " : ")
+
 
 def movies_sorted(movies):
     dic_sorted_movies = {}
@@ -48,7 +37,8 @@ def movies_sorted(movies):
         del copy_movies[highest_key]
         dic_sorted_movies[highest_key] = highest_val
     print_ranked_movies(dic_sorted_movies)
-    what_to_do(int(print_menu(back_to_menu())), movies)
+    execute_user_input(int(print_menu(back_to_menu())), movies)
+
 
 def random_movie(movies):
     movie_list = make_movie_list(movies)
@@ -56,7 +46,8 @@ def random_movie(movies):
     random_movie = movie_list[index_list]
     print("Here is a ranodm movie out of the Database:")
     print(f"Movie: {random_movie}")
-    what_to_do(int(print_menu(back_to_menu())), movies)
+    execute_user_input(int(print_menu(back_to_menu())), movies)
+
 
 def make_movie_list(movies):
     movie_list = []
@@ -64,15 +55,18 @@ def make_movie_list(movies):
         movie_list.append(key)
     return movie_list
 
+
 def make_rating_list(movies):
     value_list = []
     for key in movies:
         value_list.append(movies[key])
     return value_list
 
+
 def sort_list(list):
     list.sort(reverse=True)
     return list
+
 
 def sum_list(list):
     sum = 0
@@ -80,10 +74,12 @@ def sum_list(list):
        sum += num
     return sum
 
+
 def avergae(movies):
    value_list = make_rating_list(movies)
    average = sum_list(value_list) / len(value_list)
    return average
+
 
 def make_median(movies):
     sorted_list = sort_list(make_rating_list(movies))
@@ -98,68 +94,94 @@ def make_median(movies):
         median = sorted_list[mid_index]
     return median
 
-def best_movie(movies):
+
+def find_best_movies(movies):
     best_key = max(movies, key = movies.get)
-    best_val = movies[max(movies, key = movies.get)]
-    return best_key, best_val
+    movies_copy = movies.copy()
+    del movies_copy[best_key]
+    best_key_copy = max(movies_copy, key = movies_copy.get)
+    best_movies = {best_key : movies[best_key]}
+    while True:
+        if movies_copy[best_key_copy] == movies[best_key]:
+            best_movies[best_key_copy] = movies_copy[best_key_copy]
+            del movies_copy[best_key_copy]
+        else:
+            break
+    return best_movies
 
-def worst_movie(movies):
+
+def find_worst_movies(movies):
     worst_key = min(movies, key = movies.get)
-    worst_val = movies[min(movies, key = movies.get)]
-    return worst_key, worst_val
+    movies_copy = movies.copy()
+    del movies_copy[worst_key]
+    worst_key_copy = min(movies_copy, key=movies_copy.get)
+    worst_movies = {worst_key : movies[worst_key]}
+    while True:
+        if movies_copy[worst_key_copy] == movies[worst_key]:
+            worst_movies[worst_key_copy] = movies_copy[worst_key_copy]
+            del movies_copy[worst_key_copy]
+        else:
+            break
+    return worst_movies
 
-# statistics esten
+
 def print_statistiks(movies):
     movie_list = make_rating_list(movies)
     sorted_list = sort_list(movie_list)
     average = avergae(movies)
     median = make_median(movies)
-    best_key, best_val = best_movie(movies)
-    worst_key, worst_val = worst_movie(movies)
+    best_movies = find_best_movies(movies)
+    worst_movies = find_worst_movies(movies)
     print()
     print("The statistics are:")
     print(f"Average of rating: {average:.2f}")
     print(f"The median of rating is: {median}")
-    print(f"The best movie is: {best_key} : {best_val}")
-    print(f"The worst movie is: {worst_key} : {worst_val}")
-    what_to_do(int(print_menu(back_to_menu())), movies)
+    print("the best movie/s is/are:")
+    print_movies_and_ratings(best_movies)
+    print("the worst movie/s is/are:")
+    print_movies_and_ratings(worst_movies)
+    execute_user_input(int(print_menu(back_to_menu())), movies)
+
 
 def update_rating(movies):
     print_movies_and_ratings(movies)
     selected_movie = input("Wich of these movie ratings, you want to update? ")
     new_rating = float(input("What is the new rating?(float) "))
     movies[selected_movie] = new_rating
-    what_to_do(int(print_menu(back_to_menu())), movies)
+    execute_user_input(int(print_menu(back_to_menu())), movies)
+
 
 def delete_movie(movies):
     print_movie_list(movies)
     del_movie = input("Wich movie, from this list, you want delete?:")
     del movies[del_movie]
-    what_to_do(int(print_menu(back_to_menu())), movies)
+    execute_user_input(int(print_menu(back_to_menu())), movies)
+
 
 def add_movie(movies):
     new_movie = input("Whats the name of the movie?: ")
     rating = float(input("What is its rating?(float): "))
     movies[new_movie] = rating
-    what_to_do(int(print_menu(back_to_menu())), movies)
+    execute_user_input(int(print_menu(back_to_menu())), movies)
+
 
 def list_all_movies(movies : dict):
     print_movies_and_ratings(movies)
-    what_to_do(int(print_menu(back_to_menu())), movies)
+    execute_user_input(int(print_menu(back_to_menu())), movies)
+
 
 def print_movies_and_ratings(movies):
-    print()
-    print("********** Al Movies **********")
     for key in movies.keys():
         print(key, movies[key], sep = " : ")
 
+
 def print_movie_list(movies):
     print()
-    print("********** Al Movies **********")
     for key in movies.keys():
         print(key)
 
-def what_to_do(user_input, movies):
+
+def execute_user_input(user_input, movies):
     if user_input == 1:
         list_all_movies(movies)
     if user_input == 2:
@@ -177,8 +199,10 @@ def what_to_do(user_input, movies):
     if user_input == 8:
         movies_sorted(movies)
 
+
 def back_to_menu():
     return input("\npress Enter to continue")
+
 
 def print_menu(user_input = ""):
     print("")
@@ -192,6 +216,7 @@ def print_menu(user_input = ""):
     print("7. Search movie")
     print("8. Movies sorted by rating")
     return input("Enter choice (1-8): ")
+
 
 def main():
   # Dictionary to store the movies and the rating
@@ -209,7 +234,8 @@ def main():
   }
   # print_menu gibt 1 - 8 aus, als string
   print("********** My Movies Database **********")
-  what_to_do(int(print_menu()), movies)
+  execute_user_input(int(print_menu()), movies)
+
 
 if __name__ == "__main__":
   main()
