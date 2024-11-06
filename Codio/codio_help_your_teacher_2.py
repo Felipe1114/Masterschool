651f8bd0ba7467e26b90400f60fb1a23c9033abd
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2024-11-06 15:05:34 +0100

    helb your teacher 2 angefangen

--- Codio/codio_help_your_teacher_2.py
+++ Codio/codio_help_your_teacher_2.py
@@ -1,7 +1,45 @@
+def get_student_info() -> dict:
+    """Takes 3 inputs from the user
+    1. Students_name
+    2. english_grade
+    3. math_grade
+
+    Returns:
+        student_information(dict): {
+        student : student_name,
+        english_grade : float
+        math_grade : float
+        }
+    """
+    student_information = {}
+    student_information["student"] = input("Enter students name: ")
+    student_information["english grade"] = get_grade("english")
+    student_information["math grade"] = get_grade("math")
+    return student_information
+
+
+def get_grade(subject:str) -> float:
+    """Takes as arg a subject
+    asks the user for an input(Grade, type:number)
+    if the input is not a number, prints: "Expected a number"
+    asks the user again for an input
+
+    Args:
+        subject(str): f.example: English
+
+    Returns:
+         grade(float): the grade of the subject
+    """
+    while True:
+        try:
+            grade = float(input(f"Enter {subject} grade: "))
+            return grade
+        except ValueError:
+            print("Expected a number")
+
 
 def main():
-
-
+    print(get_student_info())
 
 if __name__ == "__main__":
     main()
