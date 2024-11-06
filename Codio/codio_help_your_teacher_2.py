1e96da5ac9ad387fb80fb619261a21fd842a9cb5
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2024-11-06 14:33:03 +0100

    init

--- Codio/codio_help_your_teacher_2.py
+++ Codio/codio_help_your_teacher_2.py
@@ -1,14 +1,7 @@
-'''
-1.
-Create a Python function called get_grade(subject) that prompts the user to input a grade for the specified subject.
-The function should handle any invalid input gracefully.
-requierements
-Accept a parameter subject (str) representing the subject for which the grade is being input.
-Prompt the user to input a grade for the specified subject. Look at demo for guidance.
-Handle any invalid input (e.g., non-numeric grades).
-If the input was invalid, prompt the user to enter a valid grade until one is received.
-Return the grade as a float.
 
-2.
+def main():
 
-'''
+
+
+if __name__ == "__main__":
+    main()
