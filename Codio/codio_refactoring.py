b42298003b5a8054b15c73dceb1555f00371db9c
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2024-11-07 16:19:03 +0100

    funktion splitting betrieben

--- Codio/codio_refactoring.py
+++ Codio/codio_refactoring.py
@@ -1,28 +1,46 @@
+def print_result(first_num, number, sec_num, sec_num_is_prime):
+  if sec_num_is_prime:
+    print(f"The number {number} equals to the sum of {first_num} and {sec_num}")
+
+
+def if_sec_num_greater_equal_two(first_num, number, sec_num):
+  if sec_num >= 2:  # j must be greater or equal 2 to be prime
+    sec_num_is_prime = validate_prime(sec_num)
+    print_result(first_num, number, sec_num, sec_num_is_prime)
+
+
+def is_first_num_prime(first_num, first_num_is_prime, number):
+  if first_num_is_prime:
+    sec_num = number - first_num
+    if_sec_num_greater_equal_two(first_num, number, sec_num)
+
+
+def validate_prime(num_to_check) -> bool:
+  """
+
+  """
+  num_is_prime = True
+  divisor = 2
+  while divisor < num_to_check:
+    if num_to_check % divisor == 0:
+      num_is_prime = False
+    divisor += 1
+  return num_is_prime
+
+
+def find_first_num(number):
+  for first_num in range(2, number):
+    first_num_is_prime = validate_prime(first_num)
+    is_first_num_prime(first_num, first_num_is_prime, number)
+  return True
+
+
 def is_sum_of_two_primes(number):
   if number % 2 == 1:
     return False
-  # hier muss die nächste funktion hin
-  for first_num in range(2, number):
-    first_num_is_prime = True
-    # check if i is a prime
-    divisor = 2
-    while divisor<first_num:
-      if first_num % divisor == 0:
-        first_num_is_prime = False
-      divisor += 1
-    if first_num_is_prime:
-      # i is a prime, now check if j=x-i is prime
-      sec_num = number - first_num
-      if sec_num >= 2:#j must be greater or equal 2 to be prime
-        sec_num_is_prime = True
-        divisor = 2
-        while divisor < sec_num:
-          if sec_num % divisor == 0:
-            sec_num_is_prime = False
-          divisor += 1
-        if sec_num_is_prime:
-          print(f"The number {number} equals to the sum of {first_num} and {sec_num}")
-  return True
+  else:
+    find_first_num(number)
+
 
 def main():
   Number = int(input("Enter a number:"))
