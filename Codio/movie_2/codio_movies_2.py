45284e862609cabca11e7aa59026e312b97ddd50
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2024-12-03 08:29:17 +0100

    user input in main funktion eingebaut

--- Codio/movie_2/codio_movies_2.py
+++ Codio/movie_2/codio_movies_2.py
@@ -1,9 +1,94 @@
-import random
+from termios import VLNEXT
+
 from programm_modules import movie_storage as ms
 from programm_modules import movie_Menu_actions as mma
 
 
-def execute_user_input(user_input, movies):
+
+
+def main():
+  """Executes the user_input(str); contains movies(dictionary)
+
+  Args:
+  none
+
+  Returns:
+  none
+  """
+  print("********** Welcome to my Movies Database **********")
+
+  movies = ms.get_movies()
+
+  while True:
+    operations ={
+      0 : exit(),
+      1 : mma.list_movies(movies),
+      2 : ms.add_movie(),
+      3 : ms.delete_movie(),
+      4 : ms. update_movie(),
+      5 : mma.get_movie_stats(movies),
+      6 : mma.print_random_movie(movies),
+      7 : mma.search_movie(movies),
+      8 : mma.print_movies_sorted_by_rating(movies)
+    }
+
+    print_menu()
+
+
+def print_menu():
+  """Displays the Menu to the user with the input commands
+
+      Args:
+          user_input (str): the empty string ftrom "Back_to_menu"
+
+      Returns:
+          the user inpt with the command for a programm aktion(str)
+  """
+  print("\n===Menu:===\n"
+        "\t0. Exit\n"
+        "\t1. List movies\n"
+        "\t2. Add moives\n"
+        "\t3. Delete movie\n"
+        "\t4. Update movie\n"
+        "\t5. stats\n"
+        "\t6. Random movie\n"
+        "\t7. Search movie\n"
+        "\t8. Movies sorted by rating")
+
+  return get_user_input()
+
+
+def get_user_input():
+  """gets a number between 0 and 8 from user
+  If input is invalid, or out of range, an error is risen
+
+  :return: user_input(a number between 1 - 8. Will be used, to choose a programm action)
+  """
+  while True:
+    try:
+      return validade_user_input()
+    except ValueError as e:
+      print(e)
+
+
+def validade_user_input():
+  """gets an input from user and changes its type to integer.
+  If input out of range, or not valid(string), an ValueError is risen
+
+  :return: user_input(0-8)
+  """
+  user_input = int(input("What do you want to do?(0-8): "))
+
+  if 8 < user_input < 0:
+    raise ValueError("Error! Input must be between 0 and 8.")
+
+  return user_input
+
+
+
+
+# hier neuen code aufsetzten
+def execute_user_input(user_input):
   """Takes the user_input and executes one of nine aktions
 
       Args:
@@ -14,9 +99,8 @@ def execute_user_input(user_input, movies):
           none
   """
   if user_input == 0:
-    print("bye")
-    exit()
-  if user_input == 1:
+    # exit
+  elif user_input == 1:
     # list all movies
   elif user_input == 2:
     # add movie
@@ -34,51 +118,7 @@ def execute_user_input(user_input, movies):
     # movies_sorted(movies)
 
 
-def print_menu(user_input = ""):
-  """Displays the Menu to the user with the input commands
 
-      Args:
-          user_input (str): the empty string ftrom "Back_to_menu"
-
-      Returns:
-          the user inpt with the command for a programm aktion(str)
-  """
-  print("")
-  print("Menu:")
-  print("0. Exit")
-  print("1. List movies")
-  print("2. Add movie")
-  print("3. Delete movie")
-  print("4. Update movie")
-  print("5. Stats")
-  print("6. Random movie")
-  print("7. Search movie")
-  print("8. Movies sorted by rating")
-  return input("Enter choice (1-8): ")
-
-
-def main():
-  """Executes the user_input(str); contains movies(dictionary)
-
-  Args:
-  none
-
-  Returns:
-  none
-  """
-  movies = ms.get_movies()
-  operations ={
-    1 : mma.list_movies(movies),
-    2 : ms.add_movie(),
-    3 : ms.delete_movie(),
-    4 : ms. update_movie(),
-    5 : mma.get_movie_stats(movies),
-    6 : mma.print_random_movie(movies),
-    7 : mma.search_movie(movies),
-    8 : mma.print_movies_sorted_by_rating(movies)
-  }
-  # print_menu gibt 1 - 8 aus, als string
-  print("********** My Movies Database **********")
 
 
 
